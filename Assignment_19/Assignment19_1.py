55915e499d0c1376128d2520ce153a8a67ca1f20
vaishnaviajaypatange-cmd <vaishnaviajaypatange@gmail.com>
2026-01-27 00:49:33 +0530

    Add files via upload

---
+++ Assignment_19/Assignment19_1.py
@@ -0,0 +1,13 @@
+
+power = lambda x: 2 ** x
+  
+
+
+def main():
+    num = int(input("Enter number: "))
+    result = power(num)
+    print("Result:", result)
+
+
+if __name__ == "__main__":
+    main()
